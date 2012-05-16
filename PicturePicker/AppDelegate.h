8bd69204a83455bec5a606913deb6f9a7a436fee
Masahiro Morodomi <morodomi at gmail.com>
2012-05-16 15:26:58 +0900

    Initial Commit

---
+++ PicturePicker/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PicturePicker
+//
+//  Created by 政洋 諸富 on 12/05/16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PicturePicker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PicturePicker
+//
+//  Created by 政洋 諸富 on 12/05/16.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
